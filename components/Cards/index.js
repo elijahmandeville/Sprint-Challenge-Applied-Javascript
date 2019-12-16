eba0fb89b65aba717f458736f5c1417d57b925af
elijahmandeville <elijahmandeville@gmail.com>
2019-12-15 19:33:19 -0700

    completed tabs and cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,51 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+//iterate over names, use template literals to select the array in the object, forEach the array, use that data to add text content
+
+const cardsContainer = document.querySelector('.cards-container');
+
+axios
+    .get("https://lambda-times-backend.herokuapp.com/articles")
+    .then((res) => {
+        const articlesData = res.data.articles;
+
+        articlesArray = Array.from(Object.keys(articlesData));
+
+        for (let i = 0; i < articlesArray.length; i++) {
+            res["data"]["articles"][`${articlesArray[i]}`].forEach((item) => {
+                const newCard = ArticleCard(item);
+                cardsContainer.appendChild(newCard);
+            })
+        }
+    })
+    .catch((err) => {
+        console.log("You received the following error: ", err);
+    });
+
+function ArticleCard(item) {
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const authorName = document.createElement('span');
+
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    author.appendChild(authorName);
+    imgContainer.appendChild(img);
+
+    headline.textContent = item.headline;
+    authorName.textContent = item.authorName;
+    img.src = item.authorPhoto;
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    return card;
+}
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,27 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topicsClass = document.querySelector('.topics');
+
+function Tabs(obj) {
+    const tab = document.createElement('div');
+
+    tab.classList.add('tab');
+
+    return tab;
+}
+
+axios
+    .get("https://lambda-times-backend.herokuapp.com/topics")
+    .then((res) => {
+        const topics = res.data.topics;
+        topics.forEach((topic) => {
+            const newCard = Tabs(topic)
+            newCard.textContent = topic;
+            topicsClass.appendChild(newCard);
+        });
+    })
+    .catch((err) => {
+        console.log("you caught an error: ", err)
+})
